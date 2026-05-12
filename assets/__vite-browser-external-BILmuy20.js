8e9404ab09cf87655f558e2c0c54729c5940e430
Omri Galor <omrigalor@omris-mbp-9.home>
2026-05-12 17:01:33 +0200

    Updates

--- assets/__vite-browser-external-BILmuy20.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./index-C2BFZiXy.js";var t=e(((e,t)=>{t.exports={}}));export default t();
---
+++ assets/__vite-browser-external-CN1vg_tl.js
@@ -0,0 +1,1 @@
+import{t as e}from"./index-Cqmpq_yK.js";var t=e(((e,t)=>{t.exports={}}));export default t();
